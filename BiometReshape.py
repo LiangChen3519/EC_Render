f17f04e0686fc59f849a626617ce36272cc0cc39
LiangChen3519 <61409749+LiangChen3519@users.noreply.github.com>
2025-12-26 17:47:38 +0200

    eddy covariance data processing tool: - ERA5 data downloading - Nearest weather station data downloading - Biomet data gapfilling

--- BiometReshape.py
+++ BiometReshape.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
-
+import os
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Optional
-
 import pandas as pd
 
 
 @dataclass
 class BiometReshape:
     biomet_type: str
+    site: str
+    year: int
     licor_path: Optional[Path] = None
     cr3000_path: Optional[Path] = None
     fmi_path: Optional[Path] = None
@@ -21,22 +22,59 @@ class BiometReshape:
 
     # read biomet data
     def readBiomet(self):
+        '''
+        a function to read biomet data based on the biomet sensor
+        :return: a dataframe of biomet data based on the biomet type
+        '''
         if self.biomet_type.lower() == "licor":
-            self.biomet_data = self.read_licor(self.licor_path)
+            self.licor_path = Path("data/{self.site}/{self.year}/{self.biomet_type.lower()}")
+            if not self.licor_path.exists():
+                os.makedirs(self.licor_path)
+            self.biomet_data = self.read_licor()
         elif self.biomet_type.lower() == "cr3000":
-            self.biomet_data = self.read_cr3000(self.cr3000_path)
+            self.cr3000_path = Path("data/{self.site}/{self.year}/{self.cr3000_path.lower()}")
+            if not self.cr3000_path.exists():
+                os.makedirs(self.cr3000_path)
+            self.biomet_data = self.read_cr3000()
         elif self.biomet_type.lower() == "fmi":
+            self.fmi_path = Path("data/{self.site}/{self.year}/{self.fmi_path.lower()}")
+            if not self.fmi_path.exists():
+                os.makedirs(self.fmi_path)
             self.fmi_data = self.read_fmi(self.fmi_path)
         else:
-            raise ValueError("Invalid biomet type")
+            raise ValueError("Invalid biomet type at this moment")
 
-    def read_licor(self, licor_path:Path):
-        pass
+    def read_licor(self):
+       '''
+       a function to read biomet data from licor Biomet sensor
+       :param licor_path:
+       :return: a dataframe of biomet data from Biomet sensor
+       '''
+       bio_licor = pd.read_csv(self.licor_path)
+       return bio_licor
 
 
-    def read_fmi(self, fmi_path:Path):
-        pass
+    def read_fmi(self):
+        '''
+        a function to read biomet data from FMI
+        :param self: data path
+        :param fmi_path: save path
+        :return: a dataframe of biomet data from FMI
+        '''
+        bio_fmi = pd.read_csv(self.fmi_path)
+        return bio_fmi
 
 
-    def read_cr3000(self, cr3000_path):
+    def read_cr3000(self):
+        '''
+        a function to read biomet data from CR3000 campbell sensor
+        :param self: data path
+        :param cr3000_path: save path
+        :return: a dataframe of biomet data from CR3000
+        '''
+        bio_cr3000 = pd.read_csv(self.cr3000_path)
+        return bio_cr3000
+
+    def reshapeBiometData(self):
+
         pass
